76cca73d52d03c924c103c08899fdf9d0bbc2bf7
Naeem Akram <naeem.akram@aspose.com>
2016-08-18 11:18:46 +0500

    Add Set License examples

---
+++ Examples/CSharp/ApplyLicense/ApplyLicenseByPath.cs
@@ -0,0 +1,22 @@
+﻿using Aspose.CAD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.CAD.Examples.CSharp.ApplyLicense
+{
+    public class ApplyLicenseByPath
+    {
+        public static void Run() 
+        {
+            // ExStart:ApplyLicenseByPath
+            // Set path of the license file, i.e. c:\temp\
+            string dataDir = @"c:\temp\";
+
+            License license = new License();
+            license.SetLicense(dataDir + "Aspose.CAD.lic");
+            // ExEnd:ApplyLicenseByPath
+        }
+    }
+}
---
+++ Examples/CSharp/ApplyLicense/ApplyLicenseUsingFileStream.cs
@@ -0,0 +1,26 @@
+﻿using Aspose.CAD;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.CAD.Examples.CSharp.ApplyLicense
+{
+    public class ApplyLicenseUsingFileStream
+    {
+        public static void Run()
+        {
+            // ExStart:ApplyLicenseUsingFileStream
+            // Set path of the license file, i.e. c:\temp\
+            string dataDir = @"c:\temp\";
+            // Load an existing file in the stream
+            FileStream LicStream = new FileStream(dataDir + "Aspose.CAD.lic", FileMode.Open);
+
+            License license = new License();
+            license.SetLicense(LicStream);
+            // ExEnd:ApplyLicenseUsingFileStream
+        }
+
+    }
+}
