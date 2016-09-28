d6c2be1dd95b09daf1ff4add875bea018902ab5f
Naeem Akram <naeem.akram@aspose.com>
2016-09-28 12:46:17 +0500

    Update examples for version 1.1.0

--- Examples/CSharp/Exporting-DGN/ExportDGNToPdf.cs
+++ Examples/CSharp/Exporting-DGN/ExportDGNToPdf.cs
@@ -11,30 +11,37 @@ namespace Aspose.CAD.Examples.CSharp.Exporting_DGN
     {
         public static void Run()
         {
-            //ExStart:ExportDGNToPdf
-            // The path to the documents directory.
-            string MyDir = RunExamples.GetDataDir_ExportingDGN();
-            string sourceFilePath = MyDir + "Nikon_D90_Camera.dgn";
-            // Load an existing DGN file as CadImage.
-            using (Aspose.CAD.FileFormats.Cad.CadImage cadImage = (Aspose.CAD.FileFormats.Cad.CadImage)Aspose.CAD.Image.Load(sourceFilePath))
+            try
             {
-                // Create an object of CadRasterizationOptions class and define/set different properties
-                Aspose.CAD.ImageOptions.CadRasterizationOptions rasterizationOptions = new Aspose.CAD.ImageOptions.CadRasterizationOptions();
-                rasterizationOptions.PageWidth = 600;
-                rasterizationOptions.PageHeight = 300;
-                rasterizationOptions.CenterDrawing = true;
-                rasterizationOptions.ScaleMethod = Aspose.CAD.FileFormats.Cad.ScaleType.None;
-                rasterizationOptions.AutomaticLayoutsScaling = true;
+                //ExStart:ExportDGNToPdf
+                // The path to the documents directory.
+                string MyDir = RunExamples.GetDataDir_ExportingDGN();
+                string sourceFilePath = MyDir + "Nikon_D90_Camera.dgn";
+                // Load an existing DGN file as CadImage.
+                using (Aspose.CAD.FileFormats.Cad.CadImage cadImage = (Aspose.CAD.FileFormats.Cad.CadImage)Aspose.CAD.Image.Load(sourceFilePath))
+                {
+                    // Create an object of CadRasterizationOptions class and define/set different properties
+                    Aspose.CAD.ImageOptions.CadRasterizationOptions rasterizationOptions = new Aspose.CAD.ImageOptions.CadRasterizationOptions();
+                    rasterizationOptions.PageWidth = 600;
+                    rasterizationOptions.PageHeight = 300;
+                    rasterizationOptions.CenterDrawing = true;
+                    rasterizationOptions.ScaleMethod = Aspose.CAD.FileFormats.Cad.ScaleType.None;
+                    rasterizationOptions.AutomaticLayoutsScaling = true;
 
-                // Create an object of PdfOptions class as we are converting the image to PDF format and assign CadRasterizationOptions object to it.
-                Aspose.CAD.ImageOptions.PdfOptions pdfOptions = new Aspose.CAD.ImageOptions.PdfOptions();
-                pdfOptions.VectorRasterizationOptions = rasterizationOptions;
+                    // Create an object of PdfOptions class as we are converting the image to PDF format and assign CadRasterizationOptions object to it.
+                    Aspose.CAD.ImageOptions.PdfOptions pdfOptions = new Aspose.CAD.ImageOptions.PdfOptions();
+                    pdfOptions.VectorRasterizationOptions = rasterizationOptions;
 
-                // Call the save method of the CadImage class object.
-                cadImage.Save(MyDir + "ExportDGNToPdf_out_.pdf", pdfOptions);
+                    // Call the save method of the CadImage class object.
+                    cadImage.Save(MyDir + "ExportDGNToPdf_out_.pdf", pdfOptions);
+                }
+                //ExEnd:ExportDGNToPdf            
+                Console.WriteLine("\nThe DGN file exported successfully to PDF.\nFile saved at " + MyDir);
             }
-            //ExEnd:ExportDGNToPdf            
-            Console.WriteLine("\nThe DGN file exported successfully to PDF.\nFile saved at " + MyDir);
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please use valid input file." + ex.Message);
+            }
 
         }
     }
--- Examples/CSharp/Exporting-DGN/ExportDGNToRasterImage.cs
+++ Examples/CSharp/Exporting-DGN/ExportDGNToRasterImage.cs
@@ -11,30 +11,37 @@ namespace Aspose.CAD.Examples.CSharp.Exporting_DGN
     {
         public static void Run()
         {
-            //ExStart:ExportDGNToRasterImage
-            // The path to the documents directory.
-            string MyDir = RunExamples.GetDataDir_ExportingDGN();
-            string sourceFilePath = MyDir + "Nikon_D90_Camera.dgn";
-            // Load an existing DGN file as CadImage.
-            using (Aspose.CAD.FileFormats.Cad.CadImage cadImage = (Aspose.CAD.FileFormats.Cad.CadImage)Aspose.CAD.Image.Load(sourceFilePath))
+            try
             {
-                // Create an object of DgnRasterizationOptions class and define/set different properties
-                Aspose.CAD.ImageOptions.DgnRasterizationOptions rasterizationOptions = new Aspose.CAD.ImageOptions.DgnRasterizationOptions();
-                rasterizationOptions.PageWidth = 600;
-                rasterizationOptions.PageHeight = 300;
-                rasterizationOptions.CenterDrawing = true;
-                rasterizationOptions.ScaleMethod = Aspose.CAD.FileFormats.Cad.ScaleType.None;
-                rasterizationOptions.AutomaticLayoutsScaling = true;
+                //ExStart:ExportDGNToRasterImage
+                // The path to the documents directory.
+                string MyDir = RunExamples.GetDataDir_ExportingDGN();
+                string sourceFilePath = MyDir + "Nikon_D90_Camera.dgn";
+                // Load an existing DGN file as CadImage.
+                using (Aspose.CAD.FileFormats.Cad.CadImage cadImage = (Aspose.CAD.FileFormats.Cad.CadImage)Aspose.CAD.Image.Load(sourceFilePath))
+                {
+                    // Create an object of DgnRasterizationOptions class and define/set different properties
+                    Aspose.CAD.ImageOptions.DgnRasterizationOptions rasterizationOptions = new Aspose.CAD.ImageOptions.DgnRasterizationOptions();
+                    rasterizationOptions.PageWidth = 600;
+                    rasterizationOptions.PageHeight = 300;
+                    rasterizationOptions.CenterDrawing = true;
+                    rasterizationOptions.ScaleMethod = Aspose.CAD.FileFormats.Cad.ScaleType.None;
+                    rasterizationOptions.AutomaticLayoutsScaling = true;
 
-                // Create an object of JpegOptions class as we are converting the DGN to jpeg and assign DgnRasterizationOptions object to it.
-                Aspose.CAD.ImageOptionsBase options = new Aspose.CAD.ImageOptions.JpegOptions();
-                options.VectorRasterizationOptions = rasterizationOptions;
+                    // Create an object of JpegOptions class as we are converting the DGN to jpeg and assign DgnRasterizationOptions object to it.
+                    Aspose.CAD.ImageOptionsBase options = new Aspose.CAD.ImageOptions.JpegOptions();
+                    options.VectorRasterizationOptions = rasterizationOptions;
 
-                // Call the save method of the CadImage class object.
-                cadImage.Save(MyDir + "ExportDGNToRasterImage_out_.pdf", options);
+                    // Call the save method of the CadImage class object.
+                    cadImage.Save(MyDir + "ExportDGNToRasterImage_out_.pdf", options);
+                }
+                //ExEnd:ExportDGNToRasterImage            
+                Console.WriteLine("\nThe DGN file exported successfully to raster image.\nFile saved at " + MyDir);
             }
-            //ExEnd:ExportDGNToRasterImage            
-            Console.WriteLine("\nThe DGN file exported successfully to raster image.\nFile saved at " + MyDir);
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please use valid input file." + ex.Message);
+            }
 
         }
     }
