dc44a53d786a0b2eb8e5422296bf72772a09b4ba
Ikram ul Haq <ikram.haq@aspose.com>
2017-01-04 15:52:34 +0500

    Export DGN format as part of DWG & accessing Underlay DWG Flags

---
+++ Examples/CSharp/DWG-Drawings/AccessingUnderlayFlagsofDWG.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aspose.CAD;
+
+namespace Aspose.CAD.Examples.CSharp.DWG_Drawings
+{
+    class AccessingUnderlayFlagsofDWG
+    {
+
+        public static void Run()
+        {
+            //ExStart: AccessingUnderlayFlagsofDWG
+
+            // Input file name and path
+            string fileName = "BlockRefDgn.dwg";
+
+            // Load an existing DWG file and convert it into CadImage 
+            using (Aspose.CAD.FileFormats.Cad.CadImage image = (Aspose.CAD.FileFormats.Cad.CadImage)Image.Load(fileName))
+            {
+                // Go through each entity inside the DWG file
+                foreach (Aspose.CAD.FileFormats.Cad.CadObjects.CadBaseEntity entity in image.Entities)
+                {
+                    // Check if entity is of CadDgnUnderlay type
+                    if (entity is Aspose.CAD.FileFormats.Cad.CadObjects.CadDgnUnderlay)
+                    {
+                        // Access different underlay flags 
+                        Aspose.CAD.FileFormats.Cad.CadObjects.CadUnderlay underlay = entity as Aspose.CAD.FileFormats.Cad.CadObjects.CadUnderlay;
+                        Console.WriteLine(underlay.UnderlayPath);
+                        Console.WriteLine(underlay.UnderlayName);
+                        Console.WriteLine(underlay.InsertionPoint.X);
+                        Console.WriteLine(underlay.InsertionPoint.Y);
+                        Console.WriteLine(underlay.RotationAngle);
+                        Console.WriteLine(underlay.ScaleX);
+                        Console.WriteLine(underlay.ScaleY);
+                        Console.WriteLine(underlay.ScaleZ);
+                        Console.WriteLine((underlay.Flags & Aspose.CAD.FileFormats.Cad.CadObjects.UnderlayFlags.UnderlayIsOn) == Aspose.CAD.FileFormats.Cad.CadObjects.UnderlayFlags.UnderlayIsOn);
+                        Console.WriteLine((underlay.Flags & Aspose.CAD.FileFormats.Cad.CadObjects.UnderlayFlags.ClippingIsOn) == Aspose.CAD.FileFormats.Cad.CadObjects.UnderlayFlags.ClippingIsOn);
+                        Console.WriteLine((underlay.Flags & Aspose.CAD.FileFormats.Cad.CadObjects.UnderlayFlags.Monochrome) != Aspose.CAD.FileFormats.Cad.CadObjects.UnderlayFlags.Monochrome);
+                        break;
+                    }
+                }
+            }
+            //ExEnd: AccessingUnderlayFlagsofDWG
+        }
+
+    }
+}
---
+++ Examples/CSharp/Exporting-DGN/ExportDGNAsPartofDWG.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aspose.CAD;
+
+
+namespace Aspose.CAD.Examples.CSharp.Exporting_DGN
+{
+    class ExportDGNAsPartofDWG
+    {
+
+        public static void Run()
+        {
+            try
+            {
+                //ExStart: ExportDGNAsPartofDWG
+
+                // Input and Output file paths
+                string fileName = "BlockRefDgn.dwg";
+                string outPath = fileName + ".pdf";
+
+                // Create an instance of PdfOptions class as we are exporting the DWG file to PDF format
+                Aspose.CAD.ImageOptions.PdfOptions exportOptions = new Aspose.CAD.ImageOptions.PdfOptions();
+
+                // Load any existing DWG file as image and convert it to CadImage type
+                using (Aspose.CAD.FileFormats.Cad.CadImage cadImage = (Aspose.CAD.FileFormats.Cad.CadImage)Image.Load(fileName))
+                {
+                    // Go through each entity inside the DWG file
+                    foreach (Aspose.CAD.FileFormats.Cad.CadObjects.CadBaseEntity baseEntity in cadImage.Entities)
+                    {
+                        // Check if entity is an image definition
+                        if (baseEntity.TypeName == Aspose.CAD.FileFormats.Cad.CadConsts.CadEntityTypeName.DGNUNDERLAY)
+                        {
+                            Aspose.CAD.FileFormats.Cad.CadObjects.CadDgnUnderlay dgnFile = (Aspose.CAD.FileFormats.Cad.CadObjects.CadDgnUnderlay)baseEntity;
+
+                            // Get external reference to object
+                            System.Console.WriteLine(dgnFile.UnderlayPath);
+                        }
+                    }
+
+                    // Define settings for CadRasterizationOptions object
+                    exportOptions.VectorRasterizationOptions = new Aspose.CAD.ImageOptions.CadRasterizationOptions()
+                    {
+                        PageWidth = 1600,
+                        PageHeight = 1600,
+                        CenterDrawing = true,
+                        Layouts = new string[] { "Model" },
+                        ScaleMethod = Aspose.CAD.FileFormats.Cad.ScaleType.None,
+                        BackgroundColor = Color.Black,
+                        DrawType = Aspose.CAD.FileFormats.Cad.CadDrawTypeMode.UseObjectColor
+                    };
+
+                    // Export the DWG to PDF by calling Save method
+                    cadImage.Save(outPath, exportOptions);
+                }
+                //ExEnd:ExportDGNAsPartofDWG
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please use valid input file." + ex.Message);
+            }
+
+        }
+
+    }
+}
