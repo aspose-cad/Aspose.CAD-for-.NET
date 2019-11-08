04f950596fa05d114f4f559a5b74cff16751587b
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-11-08 20:48:34 +0500

    Updated examples

--- Src/ConvertingCAD/ReadingDWT.cs
+++ Src/ConvertingCAD/ReadingDWT.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Aspose.CAD;
 using Aspose.CAD.FileFormats.Cad.CadTables;
+using Aspose.CAD.FileFormats.Cad;
+using Aspose.CAD.FileFormats.Cad.CadObjects;
 
 namespace Aspose.CAD.Examples.CSharp.ConvertingCAD
 {
