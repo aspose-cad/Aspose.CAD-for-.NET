9d39c83655d7b5831cb4a7b4701d079ac6bffc6f
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2020-01-27 18:01:50 +0500

    Added example w.r.t 20.1

---
+++ Src/DWG-Drawings/DWGToDWF.cs
@@ -0,0 +1,28 @@
+﻿using Aspose.CAD.FileFormats.Cad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.CAD.Examples.CSharp.DWG_Drawings
+{
+    class DWGToDWF
+    {
+        public static void Run()
+        {
+            //ExStart:DWGToDWF
+            string MyDir = RunExamples.GetDataDir_DWGDrawings();
+
+            string inputFile = MyDir + "Line.dwg";
+            string outFile = MyDir + "Line_20.1.dwf";
+            using (var cadImage = (CadImage)Image.Load(inputFile))
+            {
+                cadImage.Save(outFile);
+            }
+
+
+            //ExEnd:DWGToDWF
+        }
+
+    }
+}
